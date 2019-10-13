37a272ef0951cfaa59035f3225347a3b0d9466ca
Douglas Adams <douglas.adams@ntrepidcorp.com>
2019-10-13 13:23:03 -0700

    Adding google analytics

--- joshuahershfield/src/main.js
+++ joshuahershfield/src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-149917676-1',
+  autoTracking: {
+    screenview: true
+  },
+  router
+})
 
 new Vue({
   router,
